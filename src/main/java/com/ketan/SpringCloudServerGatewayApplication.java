00308482d09c4dadcc27b53ba693c3c41bdd4b7d
ketan <ketan.gupta@karexpert.com>
2020-11-24 23:32:37 +0530

    add spring cloud server-gateway

---
+++ src/main/java/com/ketan/SpringCloudServerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ketan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerGatewayApplication.class, args);
+	}
+
+}
